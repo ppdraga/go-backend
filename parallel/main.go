12e5fc865a1c585fd59807b3603cf44a24f5be48
Yaroslav <ppdraga@gmail.com>
2021-09-17 12:10:54 +0300

    parallel pattern

---
+++ parallel/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+type MyObj struct {
+	ID uint64
+}
+
+func main() {
+	objs := []MyObj{{1}, {2}, {3}, {4}, {5}}
+
+	expectations := make(chan chan *uint64, len(objs))
+
+	for _, obj := range objs {
+		expectation := make(chan *uint64, 1)
+		expectations <- expectation
+		go func(obj MyObj) {
+			fmt.Println(obj)
+			if obj.ID%2 != 0 {
+				expectation <- &obj.ID
+			} else {
+				expectation <- nil
+			}
+		}(obj)
+	}
+	close(expectations)
+
+	results := []uint64{}
+	for expectation := range expectations {
+		if err := <-expectation; err != nil {
+			results = append(results, *err)
+		}
+	}
+
+	fmt.Println(results)
+}
